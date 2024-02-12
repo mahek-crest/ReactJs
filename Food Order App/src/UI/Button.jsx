6c652acb604fd6f784c3b91799197a6a9448df8d
mahek-crest <mahek.g@crestinfosystems.com>
2024-02-12 17:08:58 +0530

    react-redux

--- Food Order App/src/UI/Button.jsx
+++
@@ -1,9 +0,0 @@
-export default function Button({ children, textOnly, className, ...props }) {
-  const cssClasses = textOnly ? `text-button` : `button`;
-
-  return (
-    <button className={cssClasses} {...props}>
-      {children}
-    </button>
-  );
-}
