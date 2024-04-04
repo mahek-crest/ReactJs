e42170d1c14fcac308ddcf749c2fe39829530c5a
Mahek Goswami <mahek.g@crestinfosystems.com>
2024-04-04 13:00:21 +0530

    adding dummy backend

--- ReactPr- Nextjs/src/components/PostsList.jsx
+++ ReactPr- Nextjs/src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Post from "./Post";
 import NewPost from "./NewPost";
@@ -7,8 +7,25 @@ import Modal from "./Modal";
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isfetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((existingPost) => [postData, ...existingPost]);
   }
 
@@ -19,17 +36,22 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isfetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.author} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isfetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>Nothing to show</h2>
           <p>Start adding new post..</p>
+        </div>
+      )}
+      {isfetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading...</p>
         </div>
       )}
     </>
